79ff136165dbe24ad38a9363cc7b81e0679fa7b2
Ezra Serebrin <ezraserebrin@Ezras-MBP.attlocal.net>
2020-03-22 21:44:20 -0500

    Added reset Vector, etc.

--- src/main/scala/eserebrin/emulator/CPU.scala
+++ src/main/scala/eserebrin/emulator/CPU.scala
@@ -1,22 +1,31 @@
 package eserebrin.emulator
 
 class CPU(cartridge: Cartridge) {
-  var PC = 0x0000 // eventually: 0xFFFC
+  val CPU_CLOCK_SPEED = 1789773 //TODO: implement clock
+
+  private var PC: Int = 0xFFFC
+  implicit def hex2int(hex: String): Int = Integer.parseInt(hex, 16)
+  PC = hex2int(
+    cartridge.rom(PC + 1).toHexString +
+      cartridge.rom(PC).toHexString
+  ) // Reset Vector
 
   while (true) {
-    if (PC >= 0xFFFF) {
-      System.exit(0)
+    if (scala.io.StdIn.readLine == "") {
+      if (PC >= 0xFFFA) {
+        System.exit(0)
+      }
+      println(PC.toHexString + "   " + cartridge.rom(PC).toHexString)
+      executeInstructions(cartridge.rom(PC))
+      PC += 1
     }
-    executeInstructions(cartridge.rom(PC))
-    PC += 1
-    println(PC.toHexString)
   }
 
   object Instructions {
     def NOP: Unit = {}
   }
 
-  def executeInstructions(opcode: Byte): Unit = opcode match {
+  def executeInstructions(opcode: Short): Unit = opcode match {
     case 0xEA => Instructions.NOP
   }
 }
--- src/main/scala/eserebrin/emulator/Cartridge.scala
+++ src/main/scala/eserebrin/emulator/Cartridge.scala
@@ -4,6 +4,7 @@ import java.io.File
 import scala.io.Source
 
 class Cartridge() {
-  val fp = Source.fromFile("test.txt")
-  val rom: Array[Byte] = fp.mkString.split("\\s+").map(_.toByte)
+  // val fp = Source.fromFile("test.txt")
+  // val rom: Array[Byte] = fp.mkString.split("\\s+").map(_.toByte)
+  val rom = Array.fill(0xFFFF)(0xEA.toShort)
 }
