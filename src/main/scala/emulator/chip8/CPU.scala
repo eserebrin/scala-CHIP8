d7500d387f02ccdfe122366512bb7ae5bab6effe
Ezra Serebrin <ezraserebrin@Ezras-MBP.attlocal.net>
2020-05-01 22:50:27 -0500

    Worked on opcodes, etc.

--- src/main/scala/emulator/chip8/CPU.scala
+++ src/main/scala/emulator/chip8/CPU.scala
@@ -2,85 +2,238 @@ package emulator.chip8
 
 import scalafx.scene.canvas.GraphicsContext
 import scalafx.scene.paint.Color
+import scala.util.Random
+import scala.collection.mutable
+import scalafx.scene.input.KeyCode
 
 class CPU(var memory:Array[Char] = Array.fill(0x1000)(0x00)) {
 
-    implicit def chToInt(ch:Char): Int = ch.toInt
-    implicit def intToCh(int:Int): Char = int.toChar
-    implicit def intToBool(int:Int): Boolean = {
-        if(int == 0) false
-        else true
-    }
+	implicit def chToInt(ch:Char): Int = ch.toInt
+	implicit def intToCh(int:Int): Char = int.toChar
+	implicit def intToBool(int:Int): Boolean = {
+		if(int == 0) false
+		else true
+	}
 
-    private var V = Array.fill[Char](16)(0x00)
-    private var I:Char = 0x00
-    private var PC:Char = 0x00
-    private var soundTimer = 0
-    private var delayTimer = 0
-    private var keys = Array.fill[Char](16)(0x00)
-    private var gfx = Array.fill(64*32)(false)
+	private var V = Array.fill[Char](16)(0x00)
+	private var I:Char = 0x00
+	private var PC:Char = 0x00
+	private var soundTimer = 60
+	var delayTimer = 60
+	private var keys = Array.fill[Boolean](16)(false)
+	private var gfx = Array.fill(64*32)(false)
+	private object Stack {
+		private var arr = Array.fill[Char](16)(0x0000)
+		private var sp = 0
 
-    V(0) = 32
-    V(1) = 16
-    memory(0x0000) = 0xAA
-    memory(0x0001) = 0x55
-    memory(0x0002) = 0xAA
-    memory(0x0003) = 0x55
-    memory(0x0004) = 0xD0
-    memory(0x0005) = 0x14 // draw a 4px height sprite at (32,16)
+		def push(adr: Char): Unit = {
+			sp += 1
+			arr(sp) = adr
+		}
 
-    def processGraphics(g:GraphicsContext): Unit = {
-        g.setFill(Color.Black)
-        g.fillRect(0,0,64*8,32*8)
-        var x = 0
-        var y = 0
-        for (i <- 0 until gfx.size) {
-            x = i % 64
-            y = i / 64
-            g.setFill(Color.White)
-            if(gfx(i)) g.fillRect(x*8, y*8, 8, 8)
-        }
-    }
+		def pop(): Char = {
+			var ret = arr(sp)
+			sp -= 1
+			ret
+		}
+	}
 
-    def fetchOpcode(): Char = {
-        var hb = 0x0000
-        var lb = 0x0000
-        for(i <- 1 to 2) {
-            // println(PC.toHexString.toUpperCase + "   " + memory(PC).toHexString.toUpperCase)
-            if(i == 1) hb = memory(PC) << 8
-            else lb = memory(PC)
-            if(PC < 0x999) PC += 1
-        }
-        hb | lb
-    }
+	def decTimers(): Unit = {
+		soundTimer -= 1
+		delayTimer -=1
+	}
+	
+	def resetTimers(): Unit = {
+		soundTimer = 60
+		delayTimer = 60
+	}
 
-    def processInstruction(opcode: Char): Unit = opcode match {
+	// *** BEGIN TESTING CODE *** 
 
-        // case goto if (goto >= 0x1000 && goto <= 0x1FFF) => {
-        //     val adr = opcode & 0x0FFF
+	// V(0) = 32
+	// V(1) = 16
+	// memory(0x000) = 0xAA
+	// memory(0x001) = 0x55
+	// memory(0x002) = 0xAA
+	// memory(0x003) = 0x55
+	// memory(0x004) = 0xD0
+	// memory(0x005) = 0x14 // draw a 4px height sprite at (32,16)
 
-        // }
+	// memory(0x000) = 0x14
+	// memory(0x001) = 0x44 // jump to adress 0x444
 
-        case 0x00E0 => gfx = Array.fill(64*32)(false)
+	// memory(0x000) = 0x24
+	// memory(0x001) = 0x44 // call subroutine at adress 0x444
+	// memory(0x500) = 0x00
+	// memory(0x501) = 0xEE // return
 
-        case d if (d >= 0xD000 && d <= 0xDFFF) => {
-            val x = V((opcode & 0x0F00) >>> 8)
-            val y = V((opcode & 0x00F0) >>> 4)
-            val n = opcode & 0x000F
-            val oldI = I
-            for (h <- 0 until n; w <- 0 until 8) {
-                val spr = intToBool(memory(I) & (0x80 >>> w))
-                val npx = spr ^ gfx(I)
-                val loc = (y - h - 1) * 64 + x + w
-                gfx(loc) = npx
-                if (spr != npx) V(0xF) = 1
-                else V(0xF) = 0
-                if(w == 7) I += 1
-            }
-            I = oldI
-        }
+	// V(0) = 32
+	// V(1) = 16
+	// memory(0x000) = 0xAA
+	// memory(0x001) = 0x55
+	// memory(0x002) = 0xAA
+	// memory(0x003) = 0x55
+	// memory(0x004) = 0xD0
+	// memory(0x005) = 0x14 // skip 
+	println("Rom loaded:")
+	for(ch <- memory) println(ch.toHexString)
+	println("Starting Program:\n")
 
-        case _ =>
+	// *** END OF TESTING CODE *** 
 
-    }
+	def processGraphics(g:GraphicsContext): Unit = {
+		g.setFill(Color.Black)
+		g.fillRect(0,0,64*8,32*8)
+		var x = 0
+		var y = 0
+		for (i <- 0 until gfx.size) {
+			x = i % 64
+			y = i / 64
+			g.setFill(Color.White)
+			if(gfx(i)) g.fillRect(x*8, y*8, 8, 8)
+		}
+	}
+
+	def processInput(keysPressed: mutable.Set[KeyCode]): Unit = {
+		for (key <- keysPressed) {
+			key match {
+				case KeyCode.Digit0 => keys(0x0) = true
+				case KeyCode.Digit1 => keys(0x1) = true
+				case KeyCode.Digit2 => keys(0x2) = true
+				case KeyCode.Digit3 => keys(0x3) = true
+				case KeyCode.Digit4 => keys(0x4) = true
+				case KeyCode.Digit5 => keys(0x5) = true
+				case KeyCode.Digit6 => keys(0x6) = true
+				case KeyCode.Digit7 => keys(0x7) = true
+				case KeyCode.Digit8 => keys(0x8) = true
+				case KeyCode.Digit9 => keys(0x9) = true
+				case KeyCode.A		=> keys(0xA) = true
+				case KeyCode.B		=> keys(0xB) = true
+				case KeyCode.C		=> keys(0xC) = true
+				case KeyCode.D		=> keys(0xD) = true
+				case KeyCode.E		=> keys(0xE) = true
+				case KeyCode.F		=> keys(0xF) = true
+				case _ =>
+			}
+		}
+	}
+
+	def fetchOpcode(): Char = {
+		var hb = 0x0000
+		var lb = 0x0000
+		for(i <- 1 to 2) {
+			println(PC.toHexString.toUpperCase + "   " + memory(PC).toHexString.toUpperCase)
+			if(i == 1) hb = memory(PC) << 8
+			else lb = memory(PC)
+			if(PC < 0x999) PC += 1
+		}
+		hb | lb
+	}
+
+	def processInstruction(opcode: Char): Unit = opcode match {
+
+		case 0x00E0 => gfx = Array.fill(64*32)(false) // clear screen
+
+		case 0x00EE => PC = Stack.pop() // return
+
+		case jmp if (opcode >= 0x1000 && opcode <= 0x1FFF) => PC = opcode & 0x0FFF
+
+		case call if (opcode >= 0x2000 && opcode <= 0x2FFF) => {
+			Stack.push(PC)
+			PC = opcode & 0xFFF
+		}
+
+		case skipEQ if (opcode >= 0x3000 && opcode <= 0x3FFF) => {
+			val x = (opcode & 0x0F00) >>> 8 
+			val n = opcode & 0x00FF
+			if(V(x) == n) PC += 1
+		}
+		
+		case skipNEQ if (opcode >= 0x4000 && opcode <= 0x4FFF) => {
+			val x = (opcode & 0x0F00) >>> 8 
+			val n = opcode & 0x00FF
+			if(V(x) != n) PC += 1
+		}
+		
+		case skipVEQ if (opcode >= 0x5000 && opcode <= 0x4FFF) => {
+			val x = (opcode & 0x0F00) >>> 8 
+			val y = opcode & 0x00F0
+			if(V(x) == y) PC += 1
+		}
+
+		case setV if (opcode >= 0x6000 && opcode <= 0x6FFF) => {
+			val x = (opcode & 0x0F00) >>> 8
+			val n = opcode & 0x00FF
+			V(x) = n
+		}
+		
+		case incV if (opcode >= 0x7000 && opcode <= 0x7FFF) => {
+			val x = (opcode & 0x0F00) >>> 8
+			val n = opcode & 0x00FF
+			V(x) += n
+		}
+
+		case bitOps if (opcode >= 0x8000 && opcode <= 0x8FFF) => {
+			val x = (opcode & 0x0F00) >>> 8
+			val y = (opcode & 0x00F0) >>> 4
+			val op = opcode & 0x000F
+			V(x) = op match {
+				case 0x0 => V(y)
+				case 0x1 => V(x) | V(y)
+				case 0x2 => V(x) & V(y)
+				case 0x3 => V(x) ^ V(y)
+				case 0x4 => V(x) + V(y)
+				case 0x5 => V(x) - V(y)
+				case 0x6 => {
+					V(0xF) = V(x) & 0x01
+					V(x) >> 1
+				}
+				case 0x7 => V(y) - V(x)
+				case 0x8 => {
+					V(0xF) = (V(x) & 0x80) >>> 7
+					V(x) << 1
+				}
+			}
+			if(V(x) > 0xFF) V(0xF) = 1
+			if(V(x) < 0x00) V(0xF) = 0
+		}
+
+		case setI if (opcode >= 0xA000 && opcode <= 0xAFFF) => I = opcode & 0x0FFF
+
+		case jmpV0 if (opcode >= 0xB000 & opcode <= 0xBFFF) => PC = (opcode & 0x0FFF) + V(0)
+
+		case rand if (opcode >= 0xC000 & opcode <= 0xCFFF) => {
+			val x = (opcode & 0x0F00) >>> 8
+			val n = opcode & 0x00FF
+			V(x) & Random.nextInt((0 - 255) + 1)
+		}
+
+		case dispSpr if (opcode >= 0xD000 && opcode <= 0xDFFF) => {
+			val x = V((opcode & 0x0F00) >>> 8)
+			val y = V((opcode & 0x00F0) >>> 4)
+			val n = opcode & 0x000F
+			val oldI = I
+			for (h <- 0 until n; w <- 0 until 8) {
+				val spr = intToBool(memory(I) & (0x80 >>> w))
+				val npx = spr ^ gfx(I)
+				val loc = (y + h) * 64 + x + w
+				gfx(loc) = npx
+				// println("h: " + h + ", w: " + w + ", " + gfx(loc))
+				if (spr != npx) V(0xF) = 1
+				else V(0xF) = 0
+				if(w == 7) I += 1
+			}
+			I = oldI
+		}
+
+		case keyOps if(opcode >= 0xE000 && opcode <= 0xEFFF) => {
+			val x = (opcode & 0x0F00) >>> 8
+			val op = opcode & 0x00FF
+			if(op == 0x9E && keys(V(x))) PC += 1
+			if(op == 0xA1 && keys(V(x))) PC += 1
+		}
+
+		case _ =>
+
+	}
 }
--- src/main/scala/emulator/chip8/Chip8.scala
+++ src/main/scala/emulator/chip8/Chip8.scala
@@ -5,25 +5,48 @@ import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.canvas.Canvas
 import scalafx.animation.AnimationTimer
+import java.io.BufferedInputStream
+import java.io.FileInputStream
+import java.io.File
+import scala.collection.mutable
+import java.io.DataInputStream
+import scalafx.scene.input.KeyCode
+import scalafx.scene.input.KeyEvent
 
 object Chip8 extends JFXApp {
-    stage = new JFXApp.PrimaryStage {
-        title = "CHIP-8"
-        scene = new Scene(64*8,32*8) {
-            val canvas = new Canvas(width.value, height.value)
-            content = canvas
-            val g = canvas.graphicsContext2D
+	stage = new JFXApp.PrimaryStage {
+		title = "CHIP-8"
+		scene = new Scene(64*8,32*8) {
+			val canvas = new Canvas(width.value, height.value)
+			content = canvas
+			val g = canvas.graphicsContext2D
 
-            val cpu = new CPU
+			var rom = mutable.Buffer[Char]()
+			val instream = new DataInputStream(new BufferedInputStream(new FileInputStream(new File("Pong.ch8"))))
+			while(instream.available > 0) rom += instream.readChar
 
-            val timer = AnimationTimer(t => {
-                cpu.processInstruction(cpu.fetchOpcode)
-                cpu.processGraphics(g)
-            })
+			var keysPressed = mutable.Set[KeyCode]()
+			canvas.onKeyPressed = (e: KeyEvent) => keysPressed += e.code
+			canvas.onKeyReleased = (e: KeyEvent) => keysPressed -= e.code
 
-            timer.start()
-            canvas.requestFocus()
+			val cpu = new CPU(Array.fill[Char](0x200)(0x00) ++ rom ++ Array.fill[Char](0x1000 - rom.size - 0x200)(0x00))
 
-        }
-    }
+			var oldT = 0L
+			val loop = AnimationTimer(t => {
+				if(t - oldT > 1e9 / 60) {
+					cpu.processInput(keysPressed)
+					// cpu.processInstruction(cpu.fetchOpcode)
+					cpu.processGraphics(g)
+					cpu.decTimers()
+				}
+				oldT = t
+				cpu.resetTimers()
+				println(cpu.delayTimer)
+			})
+
+			loop.start()
+			canvas.requestFocus()
+
+		}
+	}
 }
